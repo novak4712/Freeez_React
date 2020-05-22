504b777ccac2b9c8cf7804e7d24f8f9b0ef39720
Sergey Sorokin <novak4803@gmail.com>
2020-05-22 09:45:45 +0300

    start work api

--- src/index.js
+++ src/index.js
@@ -14,33 +14,7 @@ import './styles/style.css'
 const App = () => {
     return (
         <CurrentThemeProvider>
-            <div className={styles.appTheme}
-                 // onMouseMove={event => {
-                 //     const cursor = document.querySelector('.cursor');
-                 //     cursor.style.left = `${event.pageX}px`;
-                 //     cursor.style.top = `${event.pageY}px`;
-                 // }}
-                 // onMouseDown={event => {
-                 //     const cursor = document.querySelector('.cursor');
-                 //     cursor.style.width = `2em`;
-                 //     cursor.style.height = `2em`;
-                 //     cursor.style.background = `red`;
-                 // }}
-                 // onMouseUp={event => {
-                 //     const cursor = document.querySelector('.cursor');
-                 //     cursor.style.width = `3em`;
-                 //     cursor.style.height = `3em`;
-                 //     cursor.style.background = ``;
-                 // }}
-                 // onMouseLeave={event => {
-                 //     const cursor = document.querySelector('.cursor');
-                 //     cursor.style.display = `none`;
-                 // }}
-                 // onMouseEnter={event => {
-                 //     const cursor = document.querySelector('.cursor');
-                 //     cursor.style.display = ``;
-                 // }}
-            >
+            <div className={`${styles.appTheme} ${styles.unselectable}`}>
                 {/*<Preloader />*/}
                 <Router>
                     <TopBar/>
