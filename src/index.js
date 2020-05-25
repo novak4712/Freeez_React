0cf1120b8dd612ac6ef8e2c5f9123be915ad8062
Sergey Sorokin <novak4803@gmail.com>
2020-05-25 18:29:58 +0300

    services

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ import 'antd/dist/antd.css';
 import TopBar from 'components/TopBar'
 import Footer from 'components/Footer'
 import Routes from 'routes'
-import Preloader from 'components/Preloader'
+//import Preloader from 'components/Preloader'
 import {CurrentThemeProvider} from 'contexts/currentTheme'
 
 import styles from './styles/appTheme.module.scss'
 import './styles/style.css'
+
 const App = () => {
     return (
         <CurrentThemeProvider>
-            <div className={`${styles.appTheme} ${styles.unselectable}`}>
-                {/*<Preloader />*/}
-                <Router>
-                    <TopBar/>
-                    <Routes/>
-                    <Footer/>
-                </Router>
-                <div className='cursor'> </div>
-            </div>
+                <div className={`${styles.appTheme} ${styles.unselectable}`}>
+                    {/*<Preloader />*/}
+                    <Router>
+                        <TopBar/>
+                        <Routes/>
+                        <Footer/>
+                    </Router>
+                </div>
         </CurrentThemeProvider>
     )
 };
